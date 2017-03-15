7ec316a19bd5d4a5c842deee9e8a5a735f76f49c
Hanna Chen <hanna@rethinkcanada.com>
2017-03-15 17:53:32 -0400

    Initial commit -- WIP

---
+++ creative-kind/ColorShapeLayer.swift
@@ -0,0 +1,129 @@
+//
+//  ColorShapeLayer.swift
+//  creative-kind
+//
+//  Created by Hanna Chen on 3/14/17.
+//  Copyright © 2017 Rethink Canada. All rights reserved.
+//
+
+import UIKit
+
+class ColorShapeLayer: CAShapeLayer {
+    
+    var trianglePath: UIBezierPath?
+    
+    override func hitTest(_ p: CGPoint) -> ColorShapeLayer? {
+        guard let trianglePath = self.trianglePath as UIBezierPath?,
+              trianglePath.cgPath.contains(p) else {
+            return nil
+        }
+        return self
+    }
+    
+    func makeTriangle(position: CGPoint, size: Float, flip: Bool = false, reverse: Bool = false) {
+        
+        // Styles
+        if reverse {
+            self.fillColor = UIColor.blue.cgColor
+        } else {
+            self.fillColor = UIColor.red.cgColor
+        }
+        self.strokeColor = UIColor.white.cgColor
+        self.lineWidth = 1
+        
+        let triangleSize = CGFloat(size)
+        let trianglePath = UIBezierPath()
+        let firstPoint: CGPoint = self.firstPoint(position: position, size: triangleSize, flip: flip, reverse: reverse)
+        let secondPoint: CGPoint = self.secondPoint(position: position, size: triangleSize, flip: flip, reverse: reverse)
+        let thirdPoint: CGPoint = self.thirdPoint(position: position, size: triangleSize, flip: flip, reverse: reverse)
+        trianglePath.move(to: firstPoint) // point #1
+        trianglePath.addLine(to: secondPoint) // point #2
+        trianglePath.addLine(to: thirdPoint) // point #3
+        trianglePath.close()
+        self.trianglePath = trianglePath
+        
+        // Constraints and position
+        self.frame = CGRect(origin: position, size: CGSize(width: triangleSize, height: triangleSize))
+        self.bounds = trianglePath.bounds
+        self.path = trianglePath.cgPath
+    }
+    
+    func firstPoint(position: CGPoint, size: CGFloat, flip: Bool = false, reverse: Bool = false)-> CGPoint {
+        guard let firstPosition = position as CGPoint?,
+              let triangleSize = size as CGFloat? else {
+                return CGPoint(x: CGFloat(0), y: CGFloat(0))
+        }
+        
+        if reverse {
+            print("reverse...")
+            /*
+             Flip: *   *  | Normal: x   *
+                          |
+                   x   *  |         *   *
+             */
+            return flip ? CGPoint(x: firstPosition.x, y: firstPosition.y + triangleSize) : firstPosition
+        } else {
+            /*
+             Position: x   *
+             
+                       *   *
+             */
+            return firstPosition
+        }
+    }
+    
+    func secondPoint(position: CGPoint, size: CGFloat, flip: Bool = false, reverse: Bool = false)-> CGPoint {
+        guard let secondPosition = position as CGPoint?,
+              let triangleSize = size as CGFloat? else {
+            return CGPoint(x: CGFloat(0), y: CGFloat(0))
+        }
+        
+        if reverse {
+            print("reverse...")
+            /*
+             Flip: *   *  | Normal: *   *
+                          |
+                   *   x  |         x   *
+              */
+            if flip {
+                self.fillColor = UIColor.green.cgColor
+            }
+            return flip ? CGPoint(x: secondPosition.x + triangleSize, y: secondPosition.y + triangleSize) : CGPoint(x: secondPosition.x, y: secondPosition.y + triangleSize)
+        } else {
+            /*
+             Flip: *   x  | Normal: *   *
+                          |
+                   *   *  |         x   *
+             */
+            if flip {
+                self.fillColor = UIColor.orange.cgColor
+            }
+            return flip ? CGPoint(x: secondPosition.x + triangleSize, y: secondPosition.y) : CGPoint(x: secondPosition.x, y: secondPosition.y + triangleSize)
+        }
+    }
+    
+    func thirdPoint(position: CGPoint, size: CGFloat, flip: Bool = false, reverse: Bool = false)-> CGPoint {
+        guard let thirdPosition = position as CGPoint?,
+              let triangleSize = size as CGFloat? else {
+                return CGPoint(x: CGFloat(0), y: CGFloat(0))
+        }
+        
+        
+        if reverse {
+            /*
+             Position: *   *
+                       
+                       x   *
+             */
+            return CGPoint(x: thirdPosition.x + triangleSize, y: thirdPosition.y)
+        } else {
+            /*
+             Position: *   x
+             
+                       *   *
+             */
+            return CGPoint(x: thirdPosition.x + triangleSize, y: triangleSize)
+        }
+    }
+    
+}
--- creative-kind/ViewController.swift
+++ creative-kind/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Outlets
+    @IBOutlet var squareView: UIView!
+    
+    // Properties
+    let gridColumns: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        tapGestureRecognizer.cancelsTouchesInView = false
+        self.squareView.addGestureRecognizer(tapGestureRecognizer)
+        self.squareView.isUserInteractionEnabled = true
+        self.squareView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.85)
+        
+        let triangleSize: Float = Float(self.squareView.frame.size.width) / (Float(self.gridColumns) / 2)
+        
+        print("container width: \(self.squareView.frame.size.width)")
+        
+        var columnIndex: Int = 0
+        let midColumn: Int = self.gridColumns / 2
+        
+        for index in 1...self.gridColumns {
+            
+            let triangle: ColorShapeLayer = ColorShapeLayer()
+            let x: CGFloat = CGFloat(Float(columnIndex) * triangleSize)
+            let position: CGPoint = CGPoint(x: x, y: 0)
+            
+            let flip: Bool = index % 2 == 0
+            var reverse: Bool = index > self.gridColumns / 2
+            
+            // Past half-way point
+            if index > midColumn {
+                reverse = true
+                // Half-way point, reverse triangle direction
+                if index != midColumn + 1 {
+                    columnIndex += index % 2 == 0 ? 1 : 0
+                }
+            } else {
+                columnIndex += index % 2 == 0 ? 1 : 0
+            }
+            
+            triangle.makeTriangle(position: position, size: triangleSize, flip: flip, reverse: reverse)
+            
+            self.squareView.layer.addSublayer(triangle)
+        }
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        guard let touch = gestureRecognizer.location(in: self.squareView) as CGPoint?,
+              let layers = self.squareView.layer.sublayers as [CALayer]? else {
+            return
+        }
+        print("View tapped \(touch)")
+        for layer in layers {
+            if let hitLayer = layer.hitTest(touch) {
+                print("Transform layer tapped! \(hitLayer)")
+                hitLayer.opacity = 0.5
+            } else {
+                layer.opacity = 1
+            }
+        }
+    }
 
 }
 
