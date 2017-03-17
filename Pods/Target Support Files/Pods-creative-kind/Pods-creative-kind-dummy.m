fdc09b433197150fb29478a987e91806515509d9
Hanna Chen <hanna@rethinkcanada.com>
2017-03-17 01:03:01 -0400

    Add Pods folder to git

---
+++ Pods/Target Support Files/Pods-creative-kind/Pods-creative-kind-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_creative_kind : NSObject
+@end
+@implementation PodsDummy_Pods_creative_kind
+@end
---
+++ Pods/Target Support Files/Pods-creative-kindUITests/Pods-creative-kindUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_creative_kindUITests : NSObject
+@end
+@implementation PodsDummy_Pods_creative_kindUITests
+@end
